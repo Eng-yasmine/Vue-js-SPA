506124e43b1771b8736f4787acc1f23eb0b110b4
eng-yasmine <yasmeen144@yahoo.com>
2026-07-27 02:49:31 +0300

    Initial commit: Add index.html, style.css, and app.js for Vue.js application setup

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            name : 'yasmeen',
+            age : 30,
+            position : 'full stack developer',
+        }
+
+    }
+}).mount('#app-root');
