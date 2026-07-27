ee352b304b13cfe4a928285148f9f238007b4909
eng-yasmine <yasmeen144@yahoo.com>
2026-07-27 04:03:48 +0300

    Enhance index.html and app.js: Refactor HTML structure for better readability, add Vue.js methods for interactivity, and introduce data properties for dynamic content.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,34 @@
 const { createApp } = Vue;
 
 createApp({
-    data(){
+    data() {
         return {
-            name : 'yasmeen',
-            age : 30,
-            position : 'full stack developer',
+            name: 'yasmeen',
+            age: 30,
+            position: 'full stack developer',
+            link: 'https://www.google.com',
+            number: 0,
         }
 
+    },
+    //how to write methods in vue.js and how to use them in the template
+    methods: {
+        sayHello() {
+            return 'helo' + this.name;
+        },
+
+        increment() {
+            this.number++;
+        },
+
+        dontwork(){
+            // return 'this is not a method';
+            console.log('this is not a method');
+
+        },
+        doOnetime(){
+            console.log("this is a one time method");
+            alert("this is a one time method");
+        },
     }
 }).mount('#app-root');
