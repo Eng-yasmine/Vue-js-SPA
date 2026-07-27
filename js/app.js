fb98a77f0505399092e7eb46fa0bb0c197937d57
eng-yasmine <yasmeen144@yahoo.com>
2026-07-28 00:11:58 +0300

    Add Codacy instructions and update .gitignore: Include guidelines for Codacy tool usage and ignore its instructions file in version control.

--- js/app.js
+++ js/app.js
@@ -1,60 +1,74 @@
 const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-            newuser: {
-                name: '',
-                age: 30,
-                email: '',
-                password: '',
-                confirmPassword: '',
-                position: 'full stack developer',
-            },
-            link: 'https://www.google.com',
-            number1: 0,
-            number2: 0,
-            mycolor: 'red',
-            age: 22,
-        }
-
-    },
-    //how to write methods in vue.js and how to use them in the template
-    methods: {
-        sayHello() {
-            return 'helo' + this.name;
+  data() {
+    return {
+      newuser: {
+        name: "",
+        age: 30,
+        email: "",
+        password: "",
+        confirmPassword: "",
+        position: "full stack developer",
+      },
+      link: "https://www.google.com",
+      number1: 0,
+      number2: 0,
+      mycolor: "red",
+      age: 22,
+      students: ["ahmed", "ali", "yazan", "basem"],
+      employees: [
+        {
+          name: "ahmed",
+          id: 1,
+          position: "backend",
         },
-
-        increment() {
-            this.number++;
+        {
+          name: "mohamed",
+          id: 2,
+          position: "front",
         },
-
-        dontwork() {
-            // return 'this is not a method';
-            console.log('this is not a method');
-
+        {
+          name: "basem",
+          id: 3,
+          position: "devops",
         },
-        doOnetime() {
-            console.log("this is a one time method");
-            alert("this is a one time method");
-        },
-        printmyname() {
-            console.log(this.name);
-
-        },
-
-        sendData() {
-            return this.newuser;
-        },
-
+      ],
+    };
+  },
+  //how to write methods in vue.js and how to use them in the template
+  methods: {
+    sayHello() {
+      return "helo" + this.name;
     },
 
-    computed: {
-        incrementNumber() {
-            return this.number1 * 2;
-        },
-        decrementNumber() {
-            return this.number2 *5;
-        },
-    }
-}).mount('#app-root');
+    increment() {
+      this.number++;
+    },
+
+    dontwork() {
+      // return 'this is not a method';
+      console.log("this is not a method");
+    },
+    doOnetime() {
+      console.log("this is a one time method");
+      alert("this is a one time method");
+    },
+    printmyname() {
+      console.log(this.name);
+    },
+
+    sendData() {
+      return this.newuser;
+    },
+  },
+
+  computed: {
+    incrementNumber() {
+      return this.number1 * 2;
+    },
+    decrementNumber() {
+      return this.number2 * 5;
+    },
+  },
+}).mount("#app-root");
