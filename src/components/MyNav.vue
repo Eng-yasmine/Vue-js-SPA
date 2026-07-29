9719c3f163b16475191c028668b210a324c6d71c
eng-yasmine <yasmeen144@yahoo.com>
2026-07-29 07:43:28 +0300

    Refactor App.vue and remove unused components: Simplify the main application layout by removing HelloWorld, TheWelcome, and associated styles, and introduce MyNav for navigation. Update routing to use HomePage instead of HomeView and remove the AboutView.

---
+++ src/components/MyNav.vue
@@ -0,0 +1,54 @@
+<template>
+<nav class="my-nav">
+      <div class="icon">{{icone}}</div>
+         <div class="logo">{{logo}}</div>
+             <div class="menu">
+              <ul>
+  <li v-for="link in menu" :key="link">
+    {{ link }}
+  </li>
+</ul>
+               
+         </div>
+</nav>
+
+
+
+</template>
+
+<script >
+export default{
+data(){
+return{
+icone:'icon',
+logo:'logo',
+menu:['home','about','my-main'],
+
+
+
+}}
+
+}
+</script>
+
+<style scoped>
+
+*{
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}
+
+
+   .my-nav{
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 10px;
+background-color:rgba(221, 111, 111, 0.87);
+
+}
+
+
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="my-main">
+    <MyNav />
+    <h1>this is my main page</h1>
+    
+    
+    
+    </div>
+
+
+</template>
+<script>
+import MyNav from '../components/MyNav.vue';
+export default{
+name:'MyMain',
+components:{
+MyNav,
+
+}
+
+}
+
+</script>
